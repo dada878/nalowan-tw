d149c2130282e2d6c44d15efcf8b3680e9c1f674
Dada878 <xx0932399@gmail.com>
2021-12-12 23:18:52 +0800

    更新

--- js/index.js
+++ js/index.js
@@ -16,3 +16,24 @@ const vu = new Vue({
 
 vu.$mount('#app')
 vu.show = true
+
+// function scrollHandle() {
+//     $el = document.getElementsByClassName("test")[0]
+//    const offset = this.$el.getBoundingClientRect();
+   
+//    const offsetTop = offset.top;
+//    const offsetBottom = offset.bottom;
+//     if (offsetTop <= window.innerHeight && offsetBottom >= 0) {
+//         setTimeout(function (){
+//             vu.showSp = true
+//             console.log(vu.showSp)
+//         }, 200);    
+        
+    
+//    } else {
+      
+
+//    }
+//  };
+
+//  window.addEventListener("scroll", this.scrollHandle, true); // 监听 监听元素是否进入/移出可视区域
